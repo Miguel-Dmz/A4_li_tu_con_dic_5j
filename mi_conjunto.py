8906fbcc683d7ff758221b337edc16c035e52d8c
Miguel Dominguez <a.22308051281173@cbtis128.edu.mx>
2024-09-11 07:46:24 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,8 @@
+print("**** EJEMPLO DE CONJUNTOS ****")
+
+tipodeportes = {"Futbol", "Box", "Padel"}
+print(tipodeportes, "\n")
+
+print("**** LISTA DE ELEMENTPOS DEL CONJUNTO, TIPODEPORTES LOOP FOR ****")
+for dominguez in tipodeportes:
+    print(dominguez)
---
+++ mi_diccionario.py
@@ -0,0 +1,12 @@
+print("**** EJEMPLO DE DICCIONARIOS ****")
+albumes = {
+  "la pantera negra": "lyke mike",
+  "sayonara": "DATA",
+  "ESTRELLA": 1964
+}
+print(albumes, "\n")
+
+print("**** LISTA DE ELEMENTPOS DEL DICCIONARIOS, ALBUMES LOOP FOR ****")
+
+for x in albumes:
+  print(x)
